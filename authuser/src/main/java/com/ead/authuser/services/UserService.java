945df19bc957602f94ae55d8e5f5168e2e9c05a7
Ezequiel <ezequielmmarques@gmail.com>
2023-03-23 14:59:04 -0300

    Create pagination in project

--- authuser/src/main/java/com/ead/authuser/services/UserService.java
+++ authuser/src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,8 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +21,7 @@ public interface UserService {
     boolean existsByUsername(String userName);
 
     boolean existsByEmail(String email);
+
+    Page<UserModel> findAll(Pageable pageable);
+
 }
