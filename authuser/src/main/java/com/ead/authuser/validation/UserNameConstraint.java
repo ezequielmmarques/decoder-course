406611e43317c5d391810a69d6bc48dcd2151278
Ezequiel <ezequielmmarques@gmail.com>
2023-03-23 07:59:55 -0300

    creating a custom validation @annotation

---
+++ authuser/src/main/java/com/ead/authuser/validation/UserNameConstraint.java
@@ -0,0 +1,22 @@
+package com.ead.authuser.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = UserNameConstraintImpl.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserNameConstraint {
+    String message() default "Invalid username";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload[]>[] payload() default {};
+
+}
---
+++ authuser/src/main/java/com/ead/authuser/validation/UserNameConstraintImpl.java
@@ -0,0 +1,17 @@
+package com.ead.authuser.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UserNameConstraintImpl implements ConstraintValidator<UserNameConstraint, String> {
+
+    @Override
+    public void initialize(UserNameConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String userName, ConstraintValidatorContext constraintValidatorContext) {
+        return userName != null && !userName.trim().isEmpty() && !userName.contains(" ");
+    }
+}
