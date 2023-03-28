1a750c7cf099e3652644d086c046427d90a60389
Ezequiel <ezequielmmarques@gmail.com>
2023-03-27 23:05:55 -0300

    Criando uma API RESTful para Course com Validação Spring Validation

---
+++ course/src/main/java/com/ead/course/constants/CourseConstants.java
@@ -0,0 +1,5 @@
+package com.ead.course.constants;
+
+public class CourseConstants {
+    public static final String COURSE_NOT_FOUND = "Course not found";
+}
---
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -0,0 +1,84 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.CourseDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.services.CourseService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.ead.course.constants.CourseConstants.COURSE_NOT_FOUND;
+
+@Tag(name = "Courses", description = "Service para CRUD de cursos")
+@RestController
+@RequestMapping("/courses")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @PostMapping
+    public ResponseEntity<Object> saveCouse(@RequestBody @Valid CourseDto courseDto) {
+        var courseModel = new CourseModel();
+        BeanUtils.copyProperties(courseDto, courseModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
+    }
+
+    @DeleteMapping("/{courseId}")
+    public ResponseEntity<Object> deleteCourse(@PathVariable("courseId") UUID courseId) {
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if (!courseModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(COURSE_NOT_FOUND);
+        }
+        courseService.delete(courseModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Course deleted successfully");
+    }
+
+    @PutMapping("/{courseId}")
+    public ResponseEntity<Object> updateCourse(@PathVariable("courseId") UUID courseId,
+                                               @RequestBody @Valid CourseDto courseDto) {
+
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if (!courseModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(COURSE_NOT_FOUND);
+        }
+        var courseModel = courseModelOptional.get();
+        courseModel.setName(courseDto.getName());
+        courseModel.setDescription(courseDto.getDescription());
+        courseModel.setImageUrl(courseDto.getImageUrl());
+        courseModel.setCourseStatus(courseDto.getCourseStatus());
+        courseModel.setCourseLevel(courseDto.getCourseLevel());
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.save(courseModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseModel>> getAllCourse() {
+        return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll());
+    }
+
+    @GetMapping("/{courseId}")
+    public ResponseEntity<Object> getOneCourse(@PathVariable("courseId") UUID courseId) {
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+        if (!courseModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(COURSE_NOT_FOUND);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(courseModelOptional);
+    }
+}
